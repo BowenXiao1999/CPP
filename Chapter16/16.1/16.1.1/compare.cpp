0d414f9266acdd7a624ccaa962d2238094b2eff4
xiaobowen <xiaobowen@bytedance.com>
2020-08-10 12:46:38 +0800

    add bin

---
+++ Chapter16/16.1/16.1.1/compare.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+template <typename T> 
+int compare(const T& a, const T& b) {
+    if (a > b) {
+        return 1;
+    }else{
+        return -1;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << compare(2, 1) << std::endl;
+    std::cout << compare("aa", "ab") << std::endl;
+    return 0;
+}
