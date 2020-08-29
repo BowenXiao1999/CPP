48c321339bba8b8b3cf14035e09adcee3a3ada98
bowenxiaoCUHKSZ <931759898@qq.com>
2020-08-29 13:19:34 +0800

    update

---
+++ Chapter13/13.1/hasptr.cpp
@@ -0,0 +1,48 @@
+//
+// Created by bowen on 2020/8/28.
+//
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+class HasPtr {
+public:
+    string* ps;
+    int i;
+    HasPtr(const string& s = string()):
+    ps(new string(s)), i(0) {}
+
+    HasPtr(const HasPtr& orig);
+    HasPtr& operator=(const HasPtr&);
+};
+
+
+// copy construct
+HasPtr::HasPtr(const HasPtr &orig) {
+    cout << "copy construct\n";
+    ps = new string (*orig.ps);
+    i = orig.i;
+
+}
+
+// copy assignment
+HasPtr& HasPtr::operator=(const HasPtr& orig) {
+    delete ps;
+    cout << "This is in copy assignment\n";
+    auto newps = new string(*orig.ps);
+    ps = newps;
+    i = orig.i;
+}
+
+HasPtr Global;
+HasPtr Test(HasPtr a) {
+    HasPtr local = a;
+    HasPtr* heap = new HasPtr(Global);
+//    *heap = Global; // this line should call copy assignment instead of copy construct
+    HasPtr arr[4] = {local, *heap};
+    return *heap;
+}
+int main() {
+    Test(Global);
+}
